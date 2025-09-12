bd814549f9470d8ec71b6e5f2ca5e0a3ec4f75e7
joshini <joshinijose786@gmail.com>
2025-09-12 16:09:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,62 +7,63 @@ const PACKAGE_ROOT_PATH = process.cwd();
 const DIST = path.resolve(PACKAGE_ROOT_PATH, './dist');
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
 
-    return {
-        plugins: [
-            react(),
-            svgr({
-                svgrOptions: { exportType: 'named', ref: true, svgo: false, titleProp: true },
-                include: '**/*.svg'
-            })
-        ],
-        build: {
-            outDir: DIST,
-            rollupOptions: {
-                output: {
-                    entryFileNames: '[name].[hash].js',
-                    chunkFileNames: '[name].[hash].js'
-                }
-            },
-            commonjsOptions: { transformMixedEsModules: true }
-        },
-        css: {
-            modules: false
-        },
-        define: {
-            'process.env': JSON.stringify(env)
-        },
-        base: '/',
-        resolve: {
-            alias: {
-                buffer: 'buffer/',
-                '@': '/src',
-                '@common': '/src/common',
-                '@common/*': '/src/common/*',
-                '@components': '/src/components',
-                '@components/*': '/src/components/*',
-                '@mocks': '/src/mocks',
-                '@mocks/*': '/src/mocks/*',
-                '@theme': '/src/theme',
-                '@theme/*': '/src/theme/*',
-                '@utils': '/src/utils',
-                '@utils/*': '/src/utils/*',
-                '@test-utils': '/src/utils/test-utils',
-                '@styled': '/src/utils/styled',
-                '@slice': '/src/slice',
-                '@slice/*': '/src/slice/*',
-                '@routes': '/src/routes',
-                '@routes/*': '/src/routes/*',
-                '@pages': '/src/pages',
-                '@pages/*': '/src/pages/*',
-                '@services/*': '/src/services/*',
-                '@services': '/src/services',
-                '@constants': '/src/constants',
-                '@constants/*': '/src/constants/*',
-                '@testData': '/src/testData',
-                '@testData/*': '/src/testData/*'
-            }
+  return {
+    plugins: [
+      react(),
+      svgr({
+        svgrOptions: { exportType: 'named', ref: true, svgo: false, titleProp: true },
+        include: '**/*.svg'
+      })
+    ],
+    build: {
+      outDir: DIST,
+      rollupOptions: {
+        output: {
+          entryFileNames: '[name].[hash].js',
+          chunkFileNames: '[name].[hash].js'
         }
-    };
+      },
+      commonjsOptions: { transformMixedEsModules: true }
+    },
+    css: {
+      modules: false
+    },
+    define: {
+      'process.env': JSON.stringify(env)
+    },
+    // ✅ Base must be "/" for Amplify hosting at root
+    base: '/',
+    resolve: {
+      alias: {
+        buffer: 'buffer/',
+        '@': '/src',
+        '@common': '/src/common',
+        '@common/*': '/src/common/*',
+        '@components': '/src/components',
+        '@components/*': '/src/components/*',
+        '@mocks': '/src/mocks',
+        '@mocks/*': '/src/mocks/*',
+        '@theme': '/src/theme',
+        '@theme/*': '/src/theme/*',
+        '@utils': '/src/utils',
+        '@utils/*': '/src/utils/*',
+        '@test-utils': '/src/utils/test-utils',
+        '@styled': '/src/utils/styled',
+        '@slice': '/src/slice',
+        '@slice/*': '/src/slice/*',
+        '@routes': '/src/routes',
+        '@routes/*': '/src/routes/*',
+        '@pages': '/src/pages',
+        '@pages/*': '/src/pages/*',
+        '@services/*': '/src/services/*',
+        '@services': '/src/services',
+        '@constants': '/src/constants',
+        '@constants/*': '/src/constants/*',
+        '@testData': '/src/testData',
+        '@testData/*': '/src/testData/*'
+      }
+    }
+  };
 });
